85fa396f6743207ddf4a08f335fe15c5b6a0dbce
Hasan Radi <hassan93radi@hotmail.com>
2022-01-19 12:40:17 +0200

    Sdffs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const movieRoutes = require('./Server/routes/moviesRoutes.js')
+const userRoutes = require('./Server/routes/usersRoutes.js')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -9,10 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const movieRoutes = require('./Server/routes/moviesRoutes.js')
-const userRoutes = require('./Server/routes/usersRoutes.js')
-
-app.use('/', movieRoutes)
-app.use('/', userRoutes)
+app.use('/movies', movieRoutes)
+app.use('/users', userRoutes)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
