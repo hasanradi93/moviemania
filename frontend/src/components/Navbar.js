1020589a02dcc2a76833046ccf90c40ed8d43fad
HadM10 <hadmak10@gmail.com>
2022-02-05 11:17:58 +0200

    c

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import '../App.css'
 import '../css/nav.css'
 function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand navbar-dark bg-dark theNavbar" style={{ borderBottom: "10px groove #870F06", boxShadow: "10px 10px 10px black" }}>
+        <nav className="navbar navbar-expand navbar-dark bg-dark theNavbar" style={{ borderBottom: "10px groove #870F06", boxShadow: "10px 10px 10px black", zIndex: "100" }}>
             <div className="navbar-nav mr-auto" style={{ textAlign: "center", width: "100%", height: "70px" }}>
                 <h1 className='moviemania' style={{ color: "white", textAlign: "center", width: "100%", }}>
                     MovieMania
